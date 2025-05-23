8436eb0eae6fb2c740e5f9e161e9a59ea15027f3
CueviTiKen <rubentrabajos01@gmail.com>
2025-05-23 20:33:01 +0200

    Cambios leves

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { useUser } from '../context/UserContext';
-import SinAccesoPage from '../pages/SinAccesoPage'; // Importa la página que has creado
+import SinAccesoPage from '../pages/SinAccesoPage';
 
 const PrivateRoute = ({ children }) => {
   const { user } = useUser();
 
   if (!user) {
-    // En lugar de redirigir, mostramos la página SinAccesoPage
     return <SinAccesoPage />;
   }
 
-  // Si hay usuario logueado, mostramos la ruta protegida
   return children;
 };
 
